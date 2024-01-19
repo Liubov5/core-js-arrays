234feb9b00eff8e51efa02aafcefd8f4032a3e1b
Liubov5 <liubov.iakovleva5@gmail.com>
2024-01-19 21:00:36 +0300

    feat: solve 2 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -179,11 +179,8 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  // const length = arr[0].length;
-  // const sameFunc = (item) => item.length === length;
-  // return arr.every(sameFunc);
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((item) => item.length === arr[0].length);
 }
 
 /**
@@ -212,8 +209,9 @@ function isValueEqualsIndex(/* arr */) {
  *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  arr.splice(index, 0, item);
+  return arr;
 }
 
 /**
@@ -227,8 +225,16 @@ function insertItem(/* arr, item, index */) {
  *    getHead([ 'a', 'b', 'c', 'd'], 3) => [ 'a', 'b', 'c' ]
  *    getHead([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  const arr2 = arr.slice(n);
+  arr2.map((item) => {
+    const index = arr.indexOf(item);
+    if (index !== -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  });
+  return arr;
 }
 
 /**
